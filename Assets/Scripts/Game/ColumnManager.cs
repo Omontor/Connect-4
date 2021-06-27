e32afc455f7da731415ef0f6b2e9a5c9b82146c8
Oliver Montor <omontor@gmail.com>
2021-06-27 14:24:16 -0500

    spawn fix and
    Eventsystem was missing.
    Fix on spawning to automatically select location

--- Assets/Scripts/Game/ColumnManager.cs
+++ Assets/Scripts/Game/ColumnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColumnManager : MonoBehaviour
 {
@@ -9,6 +10,15 @@ public class ColumnManager : MonoBehaviour
     //Column's ID
     public int number;
 
+    private void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(clickedAction);
+    }
 
+     void clickedAction()
+    {
+        Debug.Log("You just pressed column " + number);
+        gm.TakeTurn(number);
+    }
     
 }
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -12,12 +12,27 @@ public class GameManager : MonoBehaviour
     public TMP_Text currentPlayerTag;
     //Array of possible locations to instantiate checkers
     public GameObject[] spawnLocations;
+    //Array of clickable spaces
+    public GameObject[] columns;
+    //Parent for the checkers to be added
+    public GameObject CheckerParent;
 
+
+
+    private void Start()
+    {
+        //Assign id to each column
+        for (int i = 0; i < columns.Length; i++)
+        {
+            columns[i].GetComponent<ColumnManager>().number = i;
+        }
+    }
 
     //Here we instantiate the corresponding checker depending on the turn
     public void TakeTurn (int column)
     {
-        Instantiate(player1, spawnLocations[column].transform.position, Quaternion.identity);
+        var checker = Instantiate(player1, spawnLocations[column].transform.position, Quaternion.identity);
+        checker.transform.SetParent(CheckerParent.transform);
     }
 
 }

8d6599ee46e022663c5053915526ed6957ff0432
Oliver Montor <omontor@gmail.com>
2021-06-27 14:52:00 -0500

    Sprite swapping.
    added sprite swap depending on user.
    added cooldown function to avoid multiple immediate instantiating

--- Assets/Scripts/Game/ColumnManager.cs
+++ Assets/Scripts/Game/ColumnManager.cs
@@ -12,13 +12,18 @@ public class ColumnManager : MonoBehaviour
 
     private void Start()
     {
+        //Add button listener at the begining of the game
         gameObject.GetComponent<Button>().onClick.AddListener(clickedAction);
     }
 
+    //Once user clicks, we call the action on GameManager
      void clickedAction()
     {
         Debug.Log("You just pressed column " + number);
+        //Use current id to indicate which column was pressed
         gm.TakeTurn(number);
+        //Disable buttons momentarily
+        gm.StartCoroutine("CoolOffButton");
     }
     
 }
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -6,8 +6,10 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Prefabs of checkers
+    // Checker Prefabs
     public GameObject player1, player2;
+    // User Sprites
+    public GameObject playerSprite, CPUSprite;
     //Text to tell the user who has the current turn
     public TMP_Text currentPlayerTag;
     //Array of possible locations to instantiate checkers
@@ -16,8 +18,10 @@ public class GameManager : MonoBehaviour
     public GameObject[] columns;
     //Parent for the checkers to be added
     public GameObject CheckerParent;
-
-
+    // Bool to determine which player goes next
+    bool player1Turn;
+    // Time interval between turns to avoid doble clicking and glitches
+    public int timeInterval;
 
     private void Start()
     {
@@ -26,13 +30,76 @@ public class GameManager : MonoBehaviour
         {
             columns[i].GetComponent<ColumnManager>().number = i;
         }
+        //Start with player's turn
+        SwitchTurns();
     }
 
+     
     //Here we instantiate the corresponding checker depending on the turn
     public void TakeTurn (int column)
     {
-        var checker = Instantiate(player1, spawnLocations[column].transform.position, Quaternion.identity);
-        checker.transform.SetParent(CheckerParent.transform);
+        //Check to see if it's player 1's turn (depending on the bool)
+        if (player1Turn)
+        {
+            //If it is we instantiate the corresponding checker on the column
+            var checker = Instantiate(player1, spawnLocations[column].transform.position, Quaternion.identity);
+            //Once instantiated we move it as a child of Checkerpartent to keep al the checkers in one place
+            checker.transform.SetParent(CheckerParent.transform);
+        }
+        //If it's the CPU's turn we run instantiate the other checker
+        else
+        {
+               //Instantiate and move to parent
+            var checker = Instantiate(player2, spawnLocations[column].transform.position, Quaternion.identity);
+            checker.transform.SetParent(CheckerParent.transform);
+        }
+
+        //Switch turn after instantiating
+        SwitchTurns();
     }
 
+
+
+    public void SwitchTurns()
+    {
+        //Toggle bool state
+        player1Turn = !player1Turn;
+        //Change current user text
+        if (player1Turn)
+        {
+            //Change text to User
+            currentPlayerTag.text = "User";
+            playerSprite.SetActive(true);
+            CPUSprite.SetActive(false);
+        }
+        else
+        {
+            //Change text to CPU
+            currentPlayerTag.text = "CPU";
+            playerSprite.SetActive(false);
+            CPUSprite.SetActive(true);
+        }
+    }
+
+
+    public IEnumerator CoolOffButton()
+    {
+        // Disable every column by cicling trhough the array
+        for (int i = 0; i < columns.Length; i++)
+        {
+            //disable component's interactability
+            columns[i].GetComponent<Button>().interactable = false;
+            Debug.Log("Every column disabled");
+        }
+        //Wait
+        yield return new WaitForSeconds(timeInterval);
+        //Re- enable
+        for (int i = 0; i < columns.Length; i++)
+        {
+            //disable component's interactability
+            columns[i].GetComponent<Button>().interactable = true;
+            Debug.Log("Every column is re-enabled");
+        }
+
+    }
 }
